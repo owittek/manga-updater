be22b6b06773eb87abcb63fdbc1b8de5f179a278
owittek <Olivier.Wittek@web.de>
2021-08-16 16:38:12 +0200

    added mangadex support to fetch-chapter

--- src/main/fetch-chapter.ts
+++ src/main/fetch-chapter.ts
@@ -21,7 +21,12 @@ export async function fetchChapter(manga: Manga): Promise<string> {
 }
 
 function parseLatestChapterName(document: HTMLDocument, webPage: Webpage): string | undefined {
-    let chapterElement: Element | undefined;
+    const chapterElement = getChapterElement(document, webPage);
+    return chapterElement.innerText.trim();
+}
+
+function getChapterElement(document: HTMLDocument, webPage: Webpage): Element {
+    let chapterElement: Element;
     switch (webPage) {
         case Webpage.mangakakalot:
             chapterElement = document.getElementsByClassName('row')[1].getElementsByTagName('span')[0];
@@ -33,12 +38,13 @@ function parseLatestChapterName(document: HTMLDocument, webPage: Webpage): strin
             chapterElement = document.getElementsByClassName('chapter-name')[0];
             break;
 
+        case Webpage.mangadex:
+            chapterElement = document.getElementsByClassName('text-truncate')[0];
+            break;
+
         default:
             console.error(`Case for ${webPage} has not been implemented yet!`);
             break;
-    }
-    if (chapterElement === undefined) {
-        return;
-    }
-    return chapterElement.innerText.trim();
+    }   
+    return chapterElement!;
 }
