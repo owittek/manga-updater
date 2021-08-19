f3266e18cd795d3691ca92df4c2cf9d1f50fdcb1
owittek <Olivier.Wittek@web.de>
2021-08-19 15:31:26 +0200

    main now runs on start

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,38 +1,34 @@
 import { sendDiscordMessage } from "../discord-notifier/discord-notifier.ts";
 import { fetchChapter } from "./fetch-chapter.ts";
 import { logWithTime } from "./log-with-time.ts";
-import { flushDatabase, getAllMangas, updateMangaChapter } from "./redis-com.ts";
+import { getAllMangas, updateMangaChapter } from "./redis-com.ts";
 
-const main = async (flushDb?: boolean) => {
-    if (flushDb === true) {
-        await flushDatabase();
-    }
-    const mangas = await getAllMangas();
-    logWithTime('Starting!')
-    setInterval(() => {
+const mangas = await getAllMangas();
+logWithTime('Starting!')
 
-        mangas.forEach(async manga => {
-            try {
-                const latestChapterName = await fetchChapter(manga);
-                if (manga.latestChapterName === undefined) {
-                    await updateMangaChapter(manga, latestChapterName);
-                } else if (manga.latestChapterName !== latestChapterName) {
-                    const message = `${latestChapterName} of '${manga.name}' has just dropped!`;
-                    logWithTime(message);
-                    await sendDiscordMessage({
-                        title: message,
-                        url: manga.url,
-                        color: 5814783
-                    });
-                    await updateMangaChapter(manga, latestChapterName);
-                } else {
-                    // console.log(`DEBUG: Waiting for ${manga.name}...`);
-                }
-            } catch (_typeError) {
-                // console.error(typeError);
+const main = () => {
+    mangas.forEach(async manga => {
+        try {
+            const latestChapterName = await fetchChapter(manga);
+            if (manga.latestChapterName === undefined) {
+                await updateMangaChapter(manga, latestChapterName);
+            } else if (manga.latestChapterName !== latestChapterName) {
+                const message = `${latestChapterName} of '${manga.name}' has just dropped!`;
+                logWithTime(message);
+                await sendDiscordMessage({
+                    title: message,
+                    url: manga.url,
+                    color: 5814783
+                });
+                await updateMangaChapter(manga, latestChapterName);
+            } else {
+                // console.log(`DEBUG: Waiting for ${manga.name}...`);
             }
-        });
-    }, 30 * 1000);
+        } catch (_typeError) {
+            // console.error(typeError);
+        }
+    });
 }
 
-await main();
+main();
+setInterval(() => main(), 30 * 1000);
